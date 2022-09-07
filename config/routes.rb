f8b0892a23fa69a8b34a08a1b3d1db573910e5dd
keisacct <107605705+keisacct@users.noreply.github.com>
2022-09-08 07:50:21 +0900

    Generate questions scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :questions
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations', 
