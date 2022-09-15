5549aa6c6e59b68f66b89bfbaf7572873a0a7212
keisacct <107605705+keisacct@users.noreply.github.com>
2022-09-15 23:49:38 +0900

    Update エラーメッセージ表示

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
     belongs_to :user
     has_many :answers
     is_impressionable
+
+
+    validates :title, presence: true
+    validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
 
   mount_uploader :user_icon, ImageUploader
 
+  validates :username, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = User.where(email: auth.info.email).first_or_initialize(
